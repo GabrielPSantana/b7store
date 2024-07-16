9f15498837fb8724ee7b397de87ed569b1aaaab8
GabrielPSantana <gabrieldevti@gmail.com>
2024-07-16 12:24:25 -0300

    Criando as Migrations

---
+++ database/migrations/2024_07_16_121750_create_table_advertises_images.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Advertise;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advertises_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->foreignIdFor(Advertise::class);
+            $table->boolean('featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advertises_images');
+    }
+};
